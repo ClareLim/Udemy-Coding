f93bb0a5da1a2e8f859ac82364a9bb7215c59ed1
Clarelm <97339307+ClareLim@users.noreply.github.com>
2025-06-14 15:05:52 +0800

    datatypes

---
+++ app2.js
@@ -0,0 +1,23 @@
+const myName = "Clare Lim";
+
+// val are dynamic datatypes
+let val = "String";
+val = 200;
+// because first is a String, next few convert to string
+val= "100" + 100 + 100 + "100"; 
+
+// because first is a numerical, SO LATER one the only string
+val = 100 + 100 + "100";
+
+let val1;
+val = null;
+
+let a,b,c,d;
+
+b = 100
+val = false;
+console.log(b);
+
+console.log(myName);
+console.log(val);
+
