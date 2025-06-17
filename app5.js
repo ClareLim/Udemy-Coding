49db68268197059e37ff612061f4f10030fab415
Clarelm <97339307+ClareLim@users.noreply.github.com>
2025-06-17 09:59:49 +0800

    JS changes

---
+++ app5.js
@@ -0,0 +1,40 @@
+let val = 1;
+
+val  = val * 5
+val = val - 3;
+val = val / 2;
+val = val + 10 + 10 + 30;
+val = 51 % 10;
+
+// adds by 1
+val ++ ;
+
+// minus by 1;
+val --;
+
+// subtract 3 off the current value of val
+val -= 3;
+
+// add 8 on the current value of val
+val += 10;
+
+// multiply the current value of val by 5
+val *= 5;
+
+let val1 = "Clare";
+let val2 = "Lim";
+
+val = val1 + " " + val2;
+
+
+// Shorthand to concatenate strings at the back;
+val += "Techie By Day";
+console.log(val);
+
+let val3 = 10 + 10 + "10";
+console.log(val3);
+
+// check if 10 is equal 10
+let output = (10 == 10);
+output = (10 != 10);
+console.log(output);
